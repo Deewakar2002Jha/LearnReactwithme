232b4b3dc76a652298af2471d8042f2f0c13e711
Deewakar2002Jha <deewakarjha0541@gmail.com>
2026-04-25 13:26:20 +0530

    Dynamic value in React

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import movieImg from './assets/max.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <NetflixSeries />
+      <NetflixSeries />
+      <NetflixSeries />
+    </>
+  
+)
+}
+
+const NetflixSeries = () => {
+  const movies = "Dhurandhar";
+  const rating = 9.5;
+  const description =
+    "A mysterious traveler slips into the heart of Karachi's underbelly and rises through its ranks with lethal precision, only to tear the notorious ISI-Underworld nexus apart from within.";
+  const returnGenre = () =>{
+    const genre = "Action";
+    return genre;
+  } 
+  return (
+    <div className='container'>
+      <div>
+        <img className='img' src={movieImg} alt="movie" width="50%" height="40%" />
+      </div>
+      
+      <h2>Movie name: {movies}</h2>
+      <h3>Rating: {rating}</h3>
+      <p>{description}</p>
+      <button>Genre: {returnGenre()}</button>
+    </div>
+  );
+};
+export default App
